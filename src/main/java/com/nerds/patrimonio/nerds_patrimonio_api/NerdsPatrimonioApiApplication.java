c0c81158e911dd704004503e92d36f9c664375b0
sergiopascoal <sergiopascoal9@gmail.com>
2026-01-08 17:47:03 -0300

    estrutura do projeto com o Maven e arquivos de configuração.

---
+++ src/main/java/com/nerds/patrimonio/nerds_patrimonio_api/NerdsPatrimonioApiApplication.java
@@ -0,0 +1,13 @@
+package com.nerds.patrimonio.nerds_patrimonio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdsPatrimonioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdsPatrimonioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerds/patrimonio/nerds_patrimonio_api/NerdsPatrimonioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerds.patrimonio.nerds_patrimonio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NerdsPatrimonioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
